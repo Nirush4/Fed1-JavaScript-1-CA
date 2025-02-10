0ac7a18215fc9d803ded06f76c6028f7add61597
Nirush4 <nirushvj@gmail.com>
2025-02-10 15:39:36 +0100

    setup

---
+++ js/home.mjs
@@ -0,0 +1,206 @@
+import { createHTML, clearNode } from './utils.mjs';
+import { API_URL, ERROR_MESSAGE_DEFAULT, CURRENCY } from './constants.mjs';
+// import { addToCart } from './cart.mjs';
+
+const containerEl = document.querySelector('#js-products-section');
+// const sortByEl = document.querySelector('#js-sort-by');
+
+console.log(containerEl);
+let products = [];
+
+setup();
+
+function setup() {
+  // Check if the containerEl and sortByEl elements exist in the DOM
+  if (!containerEl) {
+    // Log an error message if either element is missing
+    console.error('JS cannot run!!!');
+  } else {
+    // If both elements exist, call the setup function to initialize the application
+    getProducts();
+  }
+}
+
+/**
+ * Event listener for the 'change' event on the sortByEl element.
+ * This function sorts the product list based on the selected value.
+ *
+ * @param {Event} event - The change event triggered by the sortByEl element.
+ *
+ * The function checks the value of the event target:
+ * - If the value is "asc", it calls sortByPriceDescending() to sort the product list by price in descending order.
+ * - If the value is "dec", it calls sortByPriceAscending() to sort the product list by price in ascending order.
+ *
+ * After sorting, it calls createProductsListEl(products) to rerender the sorted product list.
+ */
+// sortByEl.addEventListener('change', (event) => {
+//   const val = event.target.value;
+
+//   // Sort productlist by price - low to high
+//   if (val === 'asc') {
+//     sortByPriceDescending();
+//     // Sort productlist by price - high to low
+//   } else if (val === 'dec') {
+//     sortByPriceAscending();
+//   }
+
+//   // NOTE: we need to rerender our sorted list now;
+//   createProductsListEl(products);
+// });
+
+async function getProducts() {
+  clearNode(containerEl);
+  createLoadingSkeleton();
+
+  try {
+    const response = await fetch(API_URL);
+    const { data } = await response.json();
+    products = data;
+
+    sortByPriceDescending();
+    createProductsListEl(products);
+  } catch (error) {
+    console.error(ERROR_MESSAGE_DEFAULT, error?.message);
+  }
+}
+
+function productTemplate({
+  id,
+  title = 'Unknown Item',
+  imgUrl,
+  imgAl,
+  price = 0,
+  description = 'Missing description',
+  index,
+}) {
+  const detailsUrl = `/product-details.html?id=${id}`;
+  return `
+  <article class="section-2-gallery animate__animated animate__fadeInUp animate__delay-${index}s">
+
+            <div class="section-2-gallery-img-1 section-2-gallery-common">
+                <div class="section-2-img-div">
+                    <a href="${detailsUrl}">
+                        <img src="${imgUrl}" alt="${imgAl}" >
+                    </a>
+                </div>
+                <p class="section-2-gallery-text-p">${title}</p>
+                <div class="c-product-preview-rating">
+                <span>&#9733;</span>
+                <span>&#9733;</span>
+                <span>&#9733;</span>
+                <span>&#9733;</span>
+                <span>&#9734;</span>
+                <span>(123 reviews)</span>
+            </div>
+              <spa class="class=section-2-gallery-text-price">${price} ${CURRENCY}</spa>
+              <button class="c-add-to-cart" id="js-add-to-cart-${id}">Add to Cart</button>
+
+    </article>
+
+
+    <article class="c-product-preview-details animate__animated animate__fadeInUp animate__delay-${index}s">
+      <div class="c-product-preview-image">
+        <a href=${detailsUrl}>
+          <img src="${imgUrl}" alt="${imgAl}" />
+        </a>  
+      </div>
+
+      <div class="c-product-preview-info">
+        <h1 class="c-product-preview-title">
+          <a href=${detailsUrl}>${title}</a>
+        </h1>
+
+        <div class="c-product-preview-rating">
+          <span>&#9733;</span>
+          <span>&#9733;</span>
+          <span>&#9733;</span>
+          <span>&#9733;</span>
+          <span>&#9734;</span>
+          <span>(123 reviews)</span>
+        </div>
+        <div class="c-product-preview-price">${price} ${CURRENCY}</div>
+        <div class="c-product-preview-description">
+          <p>
+            ${description}
+          </p>
+        </div>
+        <button class="c-add-to-cart" id="js-add-to-cart-${id}">Add to Cart</button>
+      </div>
+    </article>
+ `;
+}
+
+function productSkeletonTemplate() {
+  return `
+  <article class="c-product-preview-details">
+    <div class="c-product-preview-image">
+      <div class="skeleton skeleton-image"></div>
+    </div>
+    <div class="c-product-preview-info">
+      <div class="skeleton skeleton-title"></div>
+      <div class="skeleton skeleton-rating"></div>
+      <div class="skeleton skeleton-price"></div>
+      <div class="skeleton skeleton-description"></div>
+      <div class="skeleton skeleton-button"></div>
+    </div>
+  </article>
+ `;
+}
+
+function createLoadingSkeleton(count = 3) {
+  [...Array(count)].forEach(() => {
+    const template = productSkeletonTemplate();
+    const newEl = createHTML(template);
+    containerEl.append(newEl);
+  });
+}
+
+/**
+ * Creates and appends a list of product elements to the container element.
+ *
+ * @param {Array} [list=products] - The list of products to display. Each product should be an object with the following properties:
+ * @param {string} list[].id - The unique identifier for the product.
+ * @param {string} list[].title - The title of the product.
+ * @param {Object} list[].image - The image object for the product.
+ * @param {string} list[].image.url - The URL of the product image.
+ * @param {string} list[].image.alt - The alt text for the product image.
+ * @param {number} list[].price - The price of the product.
+ * @param {string} list[].description - The description of the product.
+ */
+function createProductsListEl(list = products) {
+  clearNode(containerEl);
+
+  list.forEach(({ id, title, image, price, description }) => {
+    const template = productTemplate({
+      id,
+      title,
+      imgUrl: image.url,
+      imgAl: image.alt,
+      price,
+      description,
+    });
+
+    const newEl = createHTML(template);
+    // FIXME: Use data attribute
+    const btn = newEl.querySelector('button');
+
+    btn.addEventListener('click', () => {
+      addToCart({
+        id,
+        title,
+        imgUrl: image.url,
+        price,
+      });
+    });
+
+    containerEl.append(newEl);
+  });
+}
+
+function sortByPriceDescending(list = products) {
+  list.sort((a, b) => a.price - b.price);
+}
+
+function sortByPriceAscending(list = products) {
+  list.sort((a, b) => b.price - a.price);
+}
---
+++ js/utils.mjs
@@ -0,0 +1,20 @@
+/**
+ * This is to create DOM elementss safely:
+ * @link https://mollify.noroff.dev/content/feu1/javascript-1/module-4/create-html?nav=
+ *
+ */
+export function createHTML(template) {
+  const parser = new DOMParser();
+  const parsedDocument = parser.parseFromString(template, 'text/html');
+  return parsedDocument.body.firstChild;
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+export function clearNode(el) {
+  removeAllChildNodes(el);
+}
