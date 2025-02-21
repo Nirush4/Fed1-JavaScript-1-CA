02d49f25759fa6a632090ccb993c9661ad30cd4a
Mortmeister <morlillehaug@hotmail.com>
2025-02-21 18:19:31 +0100

    feat: checkout summary of cart

--- js/summary-of-the-cart.mjs
+++ js/summary-of-the-cart.mjs
@@ -1,9 +1,96 @@
 const summaryFormEl = document.forms.checkout;
+const orderSummaryEl = document.querySelector('#order-summary');
+const orderSummaryContainerEl = document.querySelector(
+  '#js-order-summary-container'
+);
 
+// I need to get the cart ID and link it to the products..
+getCartSummary();
+
+function getCartSummary() {
+  const cart = JSON.parse(window.localStorage.getItem('Cart')) || [];
+  const productsList = JSON.parse(localStorage.getItem('products')) || [];
+  const limitedSaleProducts =
+    JSON.parse(localStorage.getItem('productsOnSale')) || [];
+  let storedItem = window.localStorage.getItem('Cart');
+  if (storedItem.length > 0) {
+    const cart = JSON.parse(storedItem);
+
+    setCartItemToLocalStorage(cart);
+    cart.forEach((product) => {
+      let positionProduct = [...productsList, ...limitedSaleProducts].findIndex(
+        (value) => value.id === product.productId
+      );
+      let newCart = document.createElement('div');
+      newCart.classList.add('c-summary-produckt-container');
+      newCart.dataset.id = product.productId;
+
+      let info = [...productsList, ...limitedSaleProducts][positionProduct];
+      if (!info) {
+        return;
+      }
+
+      newCart.innerHTML = `
+            <div class="c-img-container">
+                  <img src="${
+                    info.image.url
+                  }" alt="picture of a model with a red jacket" />
+                </div>
+
+                <div class="c-info-container">
+                  <div class="c-header">${info.title}</div>
+                  <div class="c-pcs">
+                    <div>${info.price.toFixed(2)}kr</div>
+                    <div>Color: ${info.baseColor}</div>
+                    <div>Size: M</div>
+                    <div>Quantity:${product.quantity}</div>
+                  </div>
+                    <div class="shopping-card-count-trash">
+                    <i class="fa-regular fa-trash-can"></i>
+                  </div>
+                </div>
+
+    `;
+      orderSummaryContainerEl.appendChild(newCart);
+    });
+  }
+}
+
+function setCartItemToLocalStorage(cart = []) {
+  window.localStorage.setItem('Cart', JSON.stringify(cart));
+}
+
+// orderSummaryContainerEl.addEventListener('click', (event) => {
+//   let positionClick = event.target;
+
+//   if (positionClick.classList.contains('fa-trash-can')) {
+//     debugger;
+//     let closestParentWithDataId = positionClick.closest('[data-id]');
+//     let productId = closestParentWithDataId.dataset.id;
+//     debugger;
+//     removeItem(productId);
+//   }
+// });
+
+// function removeItem(productId) {
+//   const cart = JSON.parse(window.localStorage.getItem('Cart')) || [];
+
+//   let itemPositionInCart = cart.findIndex(
+//     (value) => value.productId === productId
+//   );
+//   if (itemPositionInCart >= 0) {
+//     cart.splice(itemPositionInCart, 1);
+//     setCartItemToLocalStorage(cart);
+//     getCartSummary();
+//     debugger;
+//   }
+// }
+
+/// Morten is above this line of code
 formData();
 
 async function formData() {
-  if (!summaryFormEl) {
+  if (!summaryFormEl || !orderSummaryEl) {
     console.warn('JS cannot run!!!');
     return;
   }
